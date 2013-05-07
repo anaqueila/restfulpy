7a88130fbe963cfb85d6abc21acd22265266a46c
filran <filran@gmail.com>
2013-05-06 21:35:42 -0300

    Falta em deletarFuncinario verificar suas vendas

---
+++ grupo07/exibirFuncionario.tpl
@@ -0,0 +1,6 @@
+<h1>Funcionário</h1>
+<h3>Código: {{codigoFuncionario}}</h3>
+<h3>Nome: {{nome}}</h3>
+<h3>Endereço: {{endereco}}</h3>
+<h3>Sexo: {{codigoFuncionario}}</h3>
+<h3>Data de Nascimento: {{datanascimento}}</h3>
--- grupo07/server.py
+++ grupo07/server.py
@@ -1,26 +1,84 @@
 #encoding:utf-8
 
-from Funcionario import Funcionario
+from bottle import route, install, template, run, get, post, request
+from bottle_sqlite import SQLitePlugin
 
-from bottle import route,run,template
-import bottle.ext.sqlite
+install(SQLitePlugin(dbfile='funcionarios.db'))
 
-app = bottle.Bottle()
-plugin = bottle.ext.sqlite.Plugin(dbfile='funcionarios.db')
-app.install(plugin)
+#Tela para cadastrar Funcionario
+@get("/cadastrar")
+def formCadasdastrarFuncionario():
+    return '''<h1>Cadastrar Funcionário</h1>
+    <form method="POST" action="/cadastrar">
+        Código: <input type="text" name="codigo"><br>
+        Nome: <input type="text" name="nome"><br>
+        Endereço: <input type="text" name="endereco"><br>
+        Sexo: <input type="radio" name="sexo" value="m">M | <input type="radio" name="sexo" value="f">F<br>
+        Data de Nascimento: <input type="text" name="datanascimento"><br>
+        <input type="Submit" value="Cadastrar">
+    </form>'''
 
-func = Funcionario()
+#Cadastrar funcinario    
+@post("/cadastrar")
+def cadasdastrarFuncionario(db):
+    codigo = request.forms.get("codigo")
+    nome = request.forms.get("nome")
+    endereco = request.forms.get("endereco")
+    sexo = request.forms.get("sexo")
+    datanascimento = request.forms.get("datanascimento")
+    
+    sql = "SELECT codigoFuncionario FROM funcionarios WHERE codigoFuncionario='%s'" %codigo
+    c = db.execute(sql)
+    if c.fetchone():
+        return "Um funcionário já possui este código!"  
+    else:
+        sql = "INSERT INTO funcionarios (codigoFuncionario,nome,endereco,sexo,datanascimento) VALUES('%s','%s','%s','%s','%s')" %(codigo,nome,endereco,sexo,datanascimento)
+        c = db.execute(sql)
+        return "Funcionário cadastrado!"
+        
+#Tela para consultar funcionario        
+@get("/consultar")
+def formConsultarFuncionario():
+    return '''<h1>Consultar Funcinário</h1>
+        <form method="POST" action="/consultar">
+            Código: <input type="text" name="codigo"><input type="Submit" value="Consultar">
+        </form>
+    '''
 
-@route("/cadastrar")
-def cadasdastrarFuncionario():
-    return func.cadastrarFuncionario()
+#Consultar funcionario  
+@post("/consultar")
+def consultarFuncionario(db):
+    codigo = request.forms.get("codigo")
+    
+    sql = "SELECT * FROM funcionarios WHERE codigoFuncionario='%s'" %codigo
+    c = db.execute(sql)
+    if c.fetchone():
+        f = c.fetchone()
+#        print f['nome']
+        return template("exibirFuncionario", codigoFuncionario=f["codigoFuncionario"], nome=f["nome"], endereco=f["endereco"], sexo=f["sexo"], datanascimento=f["datanascimento"])
+    
 
-@route("/consultar")
-def consultarFuncionario():
-    return func.consultarFuncionario()
+#Tela para deletar funcionario
+@get("/deletar")    
+def formDeletarFuncionario():
+    return '''<h1>Deletar Funcinário</h1>
+        <form method="POST" action="/deletar">
+            Código: <input type="text" name="codigo"><input type="Submit" value="Deletar">
+        </form>
+    '''
 
-@route("/deletar")    
-def deletarFuncionario():
-    return func.deletarFuncionario()
+@post("/deletar")
+def deletarFuncionario(db):
+    codigo = request.forms.get("codigo")
+    
+    sql = "SELECT * FROM funcionarios WHERE codigoFuncionario='%s'" %codigo
+    c = db.execute(sql)
+    if c.fetchone():
+        sql = "DELETE FROM funcionarios WHERE codigoFuncionario='%s'" %codigo
+        c = db.execute(sql)
+        return "Funcionário deletado."
+    else:
+        return "Não há funcinário com este código."
+
 
 run(host="localhost", port=8007, debug=True)
